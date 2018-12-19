5892d5700ba2be8de852c6744c523584fefbed81
Chris Wood <wood.d.chris@gmail.com>
2018-12-18 20:08:46 -0500

    Init Commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+echo phpinfo();
+
+/*$date = new DateTime();
+echo $date->format('M d, Y H:i:s');*/
+
+// sayHello();
+//
+// echo '<br/>';
+//
+// greet('Tony');
+
+// function sayHello(): void
+// {
+//   echo 'Hello World!';
+// }
+//
+// function greet(String $who): void
+// {
+//   echo 'Welcome ' . $who . '!';
+// }
+
+// Arrays
+// $simpleArray = [
+//   'one',
+//   'two',
+//   'three',
+// ];
+//
+// $asocArray = [
+//   'key1'  => 'one',
+//   'key2'  => 'two',
+//   'key3'  => 'three',
+// ];
+//
+// $multiArray = [
+//   'one' => [
+//     'a',
+//     'b',
+//     'c',
+//   ],
+//   'two' => [
+//     'd',
+//     'e',
+//     'f',
+//   ],
+//   'three' => [
+//     'g',
+//     'h',
+//     'i',
+//   ],
+// ];
+//
+// // Loop through the arrays
+// for ($i = 0; $i < count($simpleArray); $i++) {
+//   echo $simpleArray[$i] . '<br/>';
+// }
+//
+// echo '<br/>';
+//
+// foreach($asocArray as $key => $value) {
+//   echo $key . ' = ' . $value . '<br/>';
+// }
+//
+// echo '<br/>';
+// echo '<table border="1">';
+//
+// foreach ($multiArray as $row => $columns) {
+//   echo '<tr>';
+//
+//   foreach ($columns as $col) {
+//     echo '<td style="padding: 10px;">' . $col . '</td>';
+//   }
+// }
